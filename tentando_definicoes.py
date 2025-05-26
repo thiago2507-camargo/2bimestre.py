b39d504194f6b36ff046386a92f359fa0a8cb60e
Thiago Camargo <thiagobuchard25@gmail.com>
2025-05-26 15:23:54 -0400

    Add files via upload

--- tentando_definicoes.py
+++ tentando_definicoes.py
@@ -1,24 +1,42 @@
-compras = ["pão", "café", "leite"]
-print(compras)
+# compras = ["pão", "café", "leite"]
+# print(compras)
 
-#pode ser removida pelo nome ou pelo indice
-compras.remove("café")
+# #pode ser removida pelo nome ou pelo indice
+# compras.remove("café")
 
-compras.remove("compras[2]")
-print(compras)
+# compras.remove("compras[2]")
+# print(compras)
 
-#append acrescenta um item ao final da lista, só pode adicionar um por vez
-compras.append("açucar")
-print(compras)
+# #append acrescenta um item ao final da lista, só pode adicionar um por vez
+# compras.append("açucar")
+# print(compras)
 
-#é preciso criar uma lista nova para receber os elementos ordenados da lista antiga
-compras_ordenadas = sorted(compras)
-print(compras_ordenadas)
+# #é preciso criar uma lista nova para receber os elementos ordenados da lista antiga
+# compras_ordenadas = sorted(compras)
+# print(compras_ordenadas)
 
-for item in compras:
-    print("-", item)
-#o nome panela é um identificador dos itens dentro da lista, esses itens podem receber qualquer tipo de nome
-for panela in compras:
-    print("-", panela)    
+# for item in compras:
+#     print("-", item)
+# #o nome panela é um identificador dos itens dentro da lista, esses itens podem receber qualquer tipo de nome
+# for panela in compras:
+#     print("-", panela)    
 
+#listas= {}
+#dicionarios= {chave valor}
+#tuplas= ()
+#conjuntos= set()
 
+# teste=(1, 2, 3, 5)
+# print(teste)
+# teste.append(4)
+# print(teste)
+
+# teste=(1, 2, 3, 5)
+# print(teste)
+# teste.remove(1)
+# print(teste)
+
+# teste={1, 2, 3, 5}
+# print(teste)
+# teste.remove(1)
+# print(teste)
